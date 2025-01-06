5f1a8696b99d0f74422bc33b6577e9e4c9ae553f
LuxCoding <110750740+LuxCodingFivem@users.noreply.github.com>
2025-01-06 12:52:17 +0100

    Added GUI for Seeting

---
+++ GUI.py
@@ -0,0 +1,327 @@
+"""
+Lux_MySQLBackup GUI
+
+Copyright (c) 2024 LuxCoding
+
+This script is licensed under the MIT License.
+For full details, see the LICENSE file in the repository.
+"""
+# Import requiered Libs
+import json
+import customtkinter 
+import tkinter
+from tkinter import filedialog
+import os
+import subprocess
+
+checkbox_vars = {}
+
+# Gets the curent Script Path and store it in a Variable
+script_dir = os.path.dirname(__file__)
+
+# Function to Restart GUI
+def restart_GUI():
+    app.destroy()  
+    os.startfile(os.path.join(script_dir, 'GUI.py'))
+
+# Function to load the Config File
+def load_config():
+    try:
+        with open(os.path.join(script_dir, 'settings.json'), 'r', encoding='utf-8') as f:
+            config = json.load(f)
+            return config
+    except FileNotFoundError:
+        print('File not fount')
+        return None
+    except json.JSONDecodeError as e:
+        print(f'Error by reading JSON-File: {e}')
+        return None
+
+# fucntion to load the Language 
+def load_language(language):
+    try:
+        with open(os.path.join(script_dir, 'language.json'), 'r', encoding='utf-8') as f:
+            data = json.load(f)  # JSON laden
+            languages = list(data.keys()) 
+            
+            language_data = data.get(language)
+            
+            if language_data is None:
+                print(f"the Language: '{language}' is not awaiable.")
+                return None, languages  
+            
+            return language_data, languages  
+    except FileNotFoundError:
+        print("File 'language.json' not found")
+        return None, []
+    except json.JSONDecodeError as e:
+        print(f'Error by Reading JSON-File: {e}')
+        return None, []
+
+# function to translate 
+def translate(key, **kwargs):
+    text = translation.get(key, key)  
+    try:
+        return text.format(**kwargs) 
+    except KeyError:
+        return text
+
+# function to save the Confing to the Config file
+def save_config(config):
+    try:
+        with open(os.path.join(script_dir, 'settings.json'), "w", encoding='utf-8') as f:
+            json.dump(config, f, indent=4)
+    except Exception as e:
+        print(translate('error_by_saving_config', e=e))
+        return None
+
+# Function to save Configuration to Config File
+def save_settings():
+    # General Settings
+    config['language'] = language_dropdown_field.get()
+    config['date_format'] = date_format.get()
+    config['backup_path'] = backup_dir_entry.get()
+    config['backup_count'] = backup_count_entry.get()
+    config['mysql_exe_path'] = mysql_exe_entry.get()
+    config['mysql_dump_exe_path'] = mysql_dump_exe_entry.get()
+
+    # Database Connection
+    config['database_user'] = database_user_entry.get()
+    config['database_password'] = database_password_entry.get()
+    config['hostname'] = hostname_entry.get()
+
+    # Databases
+    config['databases'] = {}
+    for db, var in checkbox_vars.items():
+        config['databases'][db] = var.get()
+
+    # Theme
+    config['background_theme'] = background_theme_dropdown.get()
+    config['theme_color'] = theme_color_dropdown.get()
+
+    # Save Config
+    save_config(config) 
+    
+    # Restart GUI
+    restart_GUI()
+
+
+# Function to fill in values to Entrys from Settings
+def fill_in_values():
+    # General Settings 
+    date_format.delete(0, customtkinter.END)
+    date_format.insert(0, config.get('date_format'))
+
+    backup_dir_entry.delete(0, customtkinter.END)
+    backup_dir_entry.insert(0, config.get('backup_path'))
+
+    backup_count_entry.delete(0, customtkinter.END)
+    backup_count_entry.insert(0, config.get('backup_count'))
+
+    mysql_exe_entry.delete(0, customtkinter.END)
+    mysql_exe_entry.insert(0, config.get('mysql_exe_path'))
+
+    mysql_dump_exe_entry.delete(0, customtkinter.END)
+    mysql_dump_exe_entry.insert(0, config.get('mysql_dump_exe_path'))
+
+    # Database Connection
+    hostname_entry.delete(0, customtkinter.END)
+    hostname_entry.insert(0, config.get('hostname'))
+
+    database_user_entry.delete(0, customtkinter.END)
+    database_user_entry.insert(0, config.get('database_user'))
+
+    database_password_entry.delete(0, customtkinter.END)
+    database_password_entry.insert(0, config.get('database_password'))
+
+    # Databases
+    for db, value in config['databases'].items():
+        if db in checkbox_vars: 
+            checkbox_vars[db].set(value) 
+
+# Function to Select the backup Path
+def select_backup_dir():
+    path = filedialog.askdirectory(initialdir="/", title=translate('choose_backup_dir')) 
+    backup_dir_entry.delete(0, customtkinter.END)
+    backup_dir_entry.insert(0, path)
+
+# Function to Select the MySQL.exe Path
+def select_mysql_exe_dir():
+    path = filedialog.askopenfilename(initialdir="/", title=translate('choose_mysql_exe_dir'), filetypes=(("exe files", "*.exe"),("all files", "*.*"))) 
+    mysql_exe_entry.delete(0, customtkinter.END)
+    mysql_exe_entry.insert(0, path)
+
+# Function to Select the MySQLDump.exe Path
+def select_mysql_dump_exe_dir():
+    path = filedialog.askopenfilename(initialdir="/", title=translate('choose_mysql_dump_exe_dir'), filetypes=(("exe files", "*.exe"),("all files", "*.*"))) 
+    mysql_dump_exe_entry.delete(0, customtkinter.END)
+    mysql_dump_exe_entry.insert(0, path)
+
+# Funcstion to get all Databases
+def get_databases():
+    cmd = [
+        config.get('mysql_exe_path'),
+        '-h', config.get('hostname'),
+        '-u', config.get('database_password'),
+        f'--password={config.get('database_password')}',
+        '-e', 'SHOW DATABASES;'
+    ]
+    
+    result = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    if result.returncode != 0:
+        print(translate('error_reqest_database', utf8=result.stderr.decode('utf-8')))
+        return []
+    
+    databases = result.stdout.decode('utf-8').splitlines()[1:]  
+    return databases
+
+# Save the Config and Translation in Varaibles
+config = load_config()
+translation, languages = load_language(config.get('language'))
+
+# Basic GUI Settings
+customtkinter.set_appearance_mode(config['background_theme'].lower())
+customtkinter.set_default_color_theme(config['theme_color'].lower())
+
+# Create GUI
+app = customtkinter.CTk()
+
+# Add Title and set Size
+app.title('Lux_MySQLBackup')
+app.geometry("500x500")
+
+# Create Tab View
+tabView = customtkinter.CTkTabview(app)
+tabView.pack(padx=20, pady=20)
+
+# Generall Settings
+# Add General Settings Tab to Tab View
+tabView.add(translate('general_settings'))
+
+# Language Input
+language_dropdown_label = customtkinter.CTkLabel(master=tabView.tab(translate('general_settings')), text=translate('language'))
+language_dropdown_label.grid(row=0, column=0, padx=(10, 5), pady=10, sticky='w')
+
+language_dropdown_field_str_var = customtkinter.StringVar(value=config.get('language'))
+language_dropdown_field = customtkinter.CTkOptionMenu(master=tabView.tab(translate('general_settings')), values=languages, variable=language_dropdown_field_str_var)
+language_dropdown_field.grid(row=0, column=1, padx=(5, 10), pady=10, sticky='w')
+
+# Date Format Input 
+date_format_label = customtkinter.CTkLabel(master=tabView.tab(translate('general_settings')), text=translate('date_format'))
+date_format_label.grid(row=1, column=0, padx=(10, 5), pady=10, sticky='w')
+
+date_format = customtkinter.CTkEntry(master=tabView.tab(translate('general_settings')), width=200, height=30, border_width=2, corner_radius=10)
+date_format.grid(row=1, column=1, padx=(5, 10), pady=10, sticky='w')
+
+# Backup Directory Input
+backup_dir_label = customtkinter.CTkLabel(master=tabView.tab(translate('general_settings')), text=translate('backup_path'))
+backup_dir_label.grid(row=2, column=0, padx=(10, 5), pady=10, sticky='w')
+
+backup_dir_entry = customtkinter.CTkEntry(master=tabView.tab(translate('general_settings')), width=200, height=30, border_width=2, corner_radius=10)
+backup_dir_entry.grid(row=2, column=1, padx=(5, 10), pady=10, sticky='w')
+
+backup_dir_button = customtkinter.CTkButton(master=tabView.tab(translate('general_settings')), text='...', width=30, command=select_backup_dir)
+backup_dir_button.grid(row=2, column=1, padx=(210, 10), pady=10, sticky='w')
+
+# Backup Count Input 
+backup_count_label = customtkinter.CTkLabel(master=tabView.tab(translate('general_settings')), text=translate('backup_count'))
+backup_count_label.grid(row=3, column=0, padx=(10, 5), pady=10, sticky='w')
+
+backup_count_entry = customtkinter.CTkEntry(master=tabView.tab(translate('general_settings')), width=200, height=30, border_width=2, corner_radius=10)
+backup_count_entry.grid(row=3, column=1, padx=(5, 10), pady=10, sticky='w')
+
+# MySQL.exe Path Input
+mysql_exe_label = customtkinter.CTkLabel(master=tabView.tab(translate('general_settings')), text=translate('mysql_exe_path'))
+mysql_exe_label.grid(row=4, column=0, padx=(10, 5), pady=10, sticky='w')
+
+mysql_exe_entry = customtkinter.CTkEntry(master=tabView.tab(translate('general_settings')), width=200, height=30, border_width=2, corner_radius=10)
+mysql_exe_entry.grid(row=4, column=1, padx=(5, 10), pady=10, sticky='w')
+
+mysql_exe_button = customtkinter.CTkButton(master=tabView.tab(translate('general_settings')), text='...', width=30, command=select_mysql_exe_dir)
+mysql_exe_button.grid(row=4, column=1, padx=(210, 10), pady=10, sticky='w')
+
+# MySQLDump.exe Path Input
+mysql_dump_exe_label = customtkinter.CTkLabel(master=tabView.tab(translate('general_settings')), text=translate('mysql_dump_exe_path'))
+mysql_dump_exe_label.grid(row=5, column=0, padx=(10, 5), pady=10, sticky='w')
+
+mysql_dump_exe_entry = customtkinter.CTkEntry(master=tabView.tab(translate('general_settings')), width=200, height=30, border_width=2, corner_radius=10)
+mysql_dump_exe_entry.grid(row=5, column=1, padx=(5, 10), pady=10, sticky='w')
+
+mysql_dump_exe_button = customtkinter.CTkButton(master=tabView.tab(translate('general_settings')), text='...', width=30, command=select_mysql_dump_exe_dir)
+mysql_dump_exe_button.grid(row=5, column=1, padx=(210, 10), pady=10, sticky='w')
+
+
+# Database Connection
+# Add Database Connection Tab to Tab View
+tabView.add(translate('database_connection'))
+
+# Hostname Input
+hostname_label = customtkinter.CTkLabel(master=tabView.tab(translate('database_connection')), text=translate('hostname'))
+hostname_label.grid(row=1, column=0, padx=(10, 5), pady=10, sticky='w')
+
+hostname_entry = customtkinter.CTkEntry(master=tabView.tab(translate('database_connection')), width=200, height=30, border_width=2, corner_radius=10)
+hostname_entry.grid(row=1, column=1, padx=(5, 10), pady=10, sticky='w')
+
+# Username Input
+database_user_label = customtkinter.CTkLabel(master=tabView.tab(translate('database_connection')), text=translate('database_user'))
+database_user_label.grid(row=2, column=0, padx=(10, 5), pady=10, sticky='w')
+
+database_user_entry = customtkinter.CTkEntry(master=tabView.tab(translate('database_connection')), width=200, height=30, border_width=2, corner_radius=10)
+database_user_entry.grid(row=2, column=1, padx=(5, 10), pady=10, sticky='w')
+
+# Password Input
+database_password_label = customtkinter.CTkLabel(master=tabView.tab(translate('database_connection')), text=translate('database_password'))
+database_password_label.grid(row=3, column=0, padx=(10, 5), pady=10, sticky='w')
+
+database_password_entry = customtkinter.CTkEntry(master=tabView.tab(translate('database_connection')), width=200, height=30, border_width=2, corner_radius=10, show='*')
+database_password_entry.grid(row=3, column=1, padx=(5, 10), pady=10, sticky='w')
+
+# Databases 
+# Add Databases Tab to Tabview
+tabView.add(translate('databases'))
+
+select_databases_label = customtkinter.CTkLabel(master=tabView.tab(translate('databases')), text=translate('choose_databases_to_backup'))
+select_databases_label.grid(row=0, column=0, columnspan=2, pady=(10, 10), padx=10, sticky="nsew")
+
+databases = get_databases()
+for idx, db in enumerate(databases):
+    label = customtkinter.CTkLabel(tabView.tab(translate('databases')), text=db, anchor="w")
+    label.grid(row=idx + 1, column=0, padx=10, pady=5, sticky="w")
+
+    var = customtkinter.BooleanVar()  # Variable für die Checkbox
+    checkbox = customtkinter.CTkCheckBox(tabView.tab(translate('databases')), text="", variable=var)
+    checkbox.grid(row=idx + 1, column=1, padx=10, pady=5)
+
+    checkbox_vars[db] = var
+
+# Theme
+# Add Theme Tab to Tabview
+tabView.add(translate('theme'))
+
+# Backgroud Theme Input
+background_theme_label = customtkinter.CTkLabel(master=tabView.tab(translate('theme')), text=translate('backgroup_theme'))
+background_theme_label.grid(row=0, column=0, padx=(10, 5), pady=10, sticky='w')
+
+background_theme_str_var = customtkinter.StringVar(value=config.get('background_theme'))
+background_theme_dropdown = customtkinter.CTkOptionMenu(master=tabView.tab(translate('theme')), values=['System', 'Dark', 'Light'], variable=background_theme_str_var)
+background_theme_dropdown.grid(row=0, column=1, padx=(5, 10), pady=10, sticky='w')
+
+# Theme Input 
+theme_color_label = customtkinter.CTkLabel(master=tabView.tab(translate('theme')), text=translate('theme_color'))
+theme_color_label.grid(row=1, column=0, padx=(10, 5), pady=10, sticky='w')
+
+theme_color_str_var = customtkinter.StringVar(value=config.get('theme_color'))
+theme_color_dropdown = customtkinter.CTkOptionMenu(master=tabView.tab(translate('theme')), values=['Blue', 'Green', 'Dark-Blue'], variable=theme_color_str_var)
+theme_color_dropdown.grid(row=1, column=1, padx=(5, 10), pady=10, sticky='w')
+
+# Add Save Button
+save_button = customtkinter.CTkButton(master=app, text=translate('save'), command=save_settings)
+save_button.place(relx=0.5, rely=0.8, anchor=tkinter.CENTER)
+
+# start the fill in and open the GUI
+fill_in_values()
+app.mainloop()
+
+
+# Todo:
+#     - Rewrite README
--- Lux_MySQLBackup.py
+++ Lux_MySQLBackup.py
@@ -8,192 +8,112 @@ For full details, see the LICENSE file in the repository.
 # Import requiered Libs
 import subprocess
 import os
+import json
 from datetime import datetime
 
-# Config
-# Create the Translation 
-locale = 'en'
+# Gets the curent Script Path and store it in a Variable
+script_dir = os.path.dirname(__file__)
 
-# Date format (%d = Day, %m = Month, %Y = Year, %H = Hour, %M = Minute, %S = Seconed)
-date_format = '%d.%m.%Y_%H.%M.%S'
+# Function to load the Config File
+def load_config():
+    try:
+        with open(os.path.join(script_dir, 'settings.json'), 'r', encoding='utf-8') as f:
+            config = json.load(f)
+            return config
+    except FileNotFoundError:
+        print("File 'settings.json' not found")
+        return None
+    except json.JSONDecodeError as e:
+        print(f'Error by Reading JSON-File: {e}')
+        return None
 
-translation = {
-    'de': {
-        'error_by_get_database': 'Fehler beim Abrufen der Datenbanken: {utf8}',
-        'delete_old_backup': 'Altes Backup gelöscht: {backup}',
-        'database_skiped': 'Datenbank {db_name} wird übersprungen.',
-        'backup_success': 'Backup erfolgreich für Datenbank {db_name}: {backup_path}',
-        'backup_failed': 'Backup fehlgeschlagen für Datenbank {db_name}: {utf8}',
-    },
-    'en': {
-        'error_by_get_database': 'Error retrieving databases: {utf8}',
-        'delete_old_backup': 'Old backup deleted: {backup}',
-        'database_skiped': 'Database {db_name} is skipped.',
-        'backup_success': 'Backup successful for database {db_name}: {backup_path}',
-        'backup_failed': 'Backup failed for database {db_name}: {utf8}',
-    },
-    'sp': {
-        'error_by_get_database': 'Error al recuperar las bases de datos: {utf8}',
-        'delete_old_backup': 'Copia de seguridad antigua eliminada: {backup}',
-        'database_skiped': 'Se omite la base de datos {db_name}.',
-        'backup_success': 'Copia de seguridad exitosa para la base de datos {db_name}: {backup_path}',
-        'backup_failed': 'Error en la copia de seguridad de la base de datos {db_name}: {utf8}',
-    },
-    'fr': {
-        'error_by_get_database': 'Erreur lors de la récupération des bases de données : {utf8}',
-        'delete_old_backup': 'Ancienne sauvegarde supprimée : {backup}',
-        'database_skiped': 'La base de données {db_name} est ignorée.',
-        'backup_success': 'Sauvegarde réussie pour la base de données {db_name} : {backup_path}',
-        'backup_failed': 'Échec de la sauvegarde pour la base de données {db_name} : {utf8}',
-    },
-    'it': {
-        'error_by_get_database': 'Errore durante il recupero dei database: {utf8}',
-        'delete_old_backup': 'Vecchio backup eliminato: {backup}',
-        'database_skiped': 'Database {db_name} saltato.',
-        'backup_success': 'Backup riuscito per il database {db_name}: {backup_path}',
-        'backup_failed': 'Backup fallito per il database {db_name}: {utf8}',
-    },
-    'pt': {
-        'error_by_get_database': 'Erro ao recuperar os bancos de dados: {utf8}',
-        'delete_old_backup': 'Backup antigo excluído: {backup}',
-        'database_skiped': 'Banco de dados {db_name} foi pulado.',
-        'backup_success': 'Backup bem-sucedido para o banco de dados {db_name}: {backup_path}',
-        'backup_failed': 'Falha no backup do banco de dados {db_name}: {utf8}',
-    },
-    'zh': {
-        'error_by_get_database': '检索数据库时出错：{utf8}',
-        'delete_old_backup': '已删除旧备份：{backup}',
-        'database_skiped': '数据库 {db_name} 被跳过。',
-        'backup_success': '数据库 {db_name} 的备份成功：{backup_path}',
-        'backup_failed': '数据库 {db_name} 的备份失败：{utf8}',
-    },
-    'ja': {
-        'error_by_get_database': 'データベースの取得中にエラーが発生しました: {utf8}',
-        'delete_old_backup': '古いバックアップを削除しました: {backup}',
-        'database_skiped': 'データベース {db_name} はスキップされました。',
-        'backup_success': 'データベース {db_name} のバックアップが成功しました: {backup_path}',
-        'backup_failed': 'データベース {db_name} のバックアップに失敗しました: {utf8}',
-    },
-    'ru': {
-        'error_by_get_database': 'Ошибка при получении баз данных: {utf8}',
-        'delete_old_backup': 'Старый бэкап удалён: {backup}',
-        'database_skiped': 'База данных {db_name} пропущена.',
-        'backup_success': 'Резервное копирование базы данных {db_name} выполнено успешно: {backup_path}',
-        'backup_failed': 'Ошибка резервного копирования базы данных {db_name}: {utf8}',
-    },
-    'tr': {
-        'error_by_get_database': 'Veritabanları alınırken hata oluştu: {utf8}',
-        'delete_old_backup': 'Eski yedekleme silindi: {backup}',
-        'database_skiped': 'Veritabanı {db_name} atlandı.',
-        'backup_success': 'Veritabanı {db_name} için yedekleme başarılı: {backup_path}',
-        'backup_failed': 'Veritabanı {db_name} için yedekleme başarısız: {utf8}',
-    },
-    'nl': {
-        'error_by_get_database': 'Fout bij het ophalen van databases: {utf8}',
-        'delete_old_backup': 'Oude back-up verwijderd: {backup}',
-        'database_skiped': 'Database {db_name} wordt overgeslagen.',
-        'backup_success': 'Back-up geslaagd voor database {db_name}: {backup_path}',
-        'backup_failed': 'Back-up mislukt voor database {db_name}: {utf8}',
-    }
-}
+# fucntion to load the Language 
+def load_language(language):
+    try:
+        with open(os.path.join(script_dir, 'language.json'), 'r', encoding='utf-8') as f:
+            data = json.load(f)  # JSON laden
+            languages = list(data.keys()) 
+            
+            language_data = data.get(language)
+            
+            if language_data is None:
+                print(f"the Language: '{language}' is not awaiable.")
+                return None
+            
+            return language_data
+    except FileNotFoundError:
+        print("File 'language.json' not found")
+        return None
+    except json.JSONDecodeError as e:
+        print(f'Error by Reading JSON-File: {e}')
+        return None
 
 # function to translate 
 def translate(key, **kwargs):
-    text = translation.get(locale, {}).get(key, key)  
+    text = translation.get(key, key)  
     try:
         return text.format(**kwargs) 
     except KeyError:
         return text
 
+# Load Config and Translation
+config = load_config()
+translation = load_language(config.get('language'))
 
-# Defining Variables for the mySQL Connection
-db_host = 'localhost'  # Hostname or IP of the Database
-db_user = ''  # Database Username
-db_password = ''  # Database Password
+# Creates Backup Directory if its not exists
+if not os.path.exists(config.get('backup_path')):
+    os.makedirs(config.get('backup_path'))
 
-# Defining Variables for Backup 
-backup_base_dir = 'C:/Users/Administrator/OneDrive/MySQL Backups'  # Where sould the Backup be stored 
-versionen_behalten = 360 # how many Backups sould be saved until the Backups will deleted 
+# Function to cleanup old Backups
+def cleanup_old_backups():
+    backups = [f for f in os.listdir(config.get('backup_path')) if f.endswith('.sql')]
+    backups.sort(key=lambda f: os.path.getmtime(os.path.join(config.get('backup_path'), f))) # Sort after last change time 
 
-# Path to mysql.exe and mysqldump.exe
-mysql_path = 'C:/Program Files/MariaDB 11.5/bin/mysql.exe'
-mysqldump_path = 'C:/Program Files/MariaDB 11.5/bin/mysqldump.exe'
-
-# Exclude Databases
-exclude_databases = ['information_schema', 'performance_schema', 'sys', 'mysql']
-
-# Create The Backup Director if its not exists 
-if not os.path.exists(backup_base_dir):
-    os.makedirs(backup_base_dir)
-
-# Funcstion to get all Databases
-def get_databases():
-    cmd = [
-        mysql_path,  
-        '-h', db_host,
-        '-u', db_user,
-        f'--password={db_password}',
-        '-e', 'SHOW DATABASES;'
-    ]
-    
-    result = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    if result.returncode != 0:
-        print(translate('error_by_get_database', utf8=result.stderr.decode('utf-8')))
-        return []
-    
-    databases = result.stdout.decode('utf-8').splitlines()[1:]  # Ignore the Headline
-    return databases
-
-# function to delete the old backups
-def cleanup_old_backups(db_backup_dir):
-    backups = [f for f in os.listdir(db_backup_dir) if f.endswith('.sql')]
-    backups.sort(key=lambda f: os.path.getmtime(os.path.join(db_backup_dir, f)))  # sort by Edit time
-
-    # Check if there is more Backups the defined 
-    if len(backups) > versionen_behalten:
-        # Calculate Backups to delete
-        old_backups = backups[:len(backups) - versionen_behalten]
+    # Check if there are more then allow count of backups
+    if len(backups) > int(config.get('backup_count')):
+        # get backups to delete
+        old_backups = backups[:len(backups) - config.get('backup_count')]
         for backup in old_backups:
-            os.remove(os.path.join(db_backup_dir, backup))
-            print(translate('delete_old_backup', backup=backup))
+            os.remove(os.path.join(config.get('backup_path'), backup))
+            print(translate('delete_old_backup_successfull', backup=backup))
 
-# Get Databesed
-databases = get_databases()
+# Funcstion to get all enabled Databases
+def get_enabled_databases():
+    databases = config.get('databases', {})
+    if isinstance(databases, dict):
+        return [db for db, enabled in databases.items() if enabled]
+    return []  
 
-# Backup the databases individually except the excluded ones
+# Get all Databases
+databases = get_enabled_databases()
+
+# Save every Database by its own
 for db_name in databases:
-    if db_name in exclude_databases:
-        print('database_skiped', db_name=db_name)
-        continue
-
-    # Create Subfolder for every Databse
-    db_backup_dir = os.path.join(backup_base_dir, db_name)
+    # Create Subfolder for every Database
+    db_backup_dir = os.path.join(config.get('backup_path'), db_name)
     if not os.path.exists(db_backup_dir):
         os.makedirs(db_backup_dir)
-    
-    # Create an file name with current time stemp
-    current_time = datetime.now().strftime(date_format)
-    backup_filename = f"{db_name}_backup_{current_time}.sql"
-    backup_path = os.path.join(db_backup_dir, backup_filename)
-    
-    # mysqldump-command to save the Databases 
+
+    # Creates File name with current date
+    current_time = datetime.now().strftime(config('date_format'))
+    backup_filename = f"{db_name}_Backup_{current_time}.sql"
+    backup_path = os.path.join(config.get('backup_path'), backup_filename)
+
+    # MySQLDump Command for every own Database
     dump_cmd = [
-        mysqldump_path,
-        '-h', db_host,
-        '-u', db_user,
-        f'--password={db_password}',
+        config.get('mysql_dump_exe_path'),
+        '-h', config.get('hostname'),
+        '-u', config.get('database_user'),
+        f'--password={config.get('database_password')}',
         db_name
     ]
-    
-    # Start the Backup and wirte it in the files
-    with open(backup_path, 'w') as f:
+
+    # Start the Backup process and write the data in the file
+    with open(backup_path, 'w', encoding='utf-8') as f:
         result = subprocess.run(dump_cmd, stdout=f, stderr=subprocess.PIPE)
-    
-    # Check for errors
+
+    # Check for Errors:
     if result.returncode == 0:
-        print(translate('backup_success', db_name=db_name, backup_path=backup_path))
+        print(translate('backup_successful', db_name=db_name, backup_path=backup_path))
     else:
         print(translate('backup_failed', db_name=db_name, utf8=result.stderr.decode('utf-8')))
-    
-    # Delete old Backups
-    cleanup_old_backups(db_backup_dir)
