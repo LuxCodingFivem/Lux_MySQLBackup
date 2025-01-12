7356a9181b6aa0fc35ccd24fd6ee5a13eea0ebf8
LuxCoding <110750740+LuxCodingFivem@users.noreply.github.com>
2025-01-12 19:49:33 +0100

    removed dependency from mysql and mysqldump
    Added lib pymysql
    Added load_database and SQL function
    Rewritten the backup function
    MySQL.exe and MySQLDump.exe are no longer needed
    Removed MySQL.exe and MySQLDump.exe input from the GUI

---
+++ Lux_MySQLBackup/Decrypt backup GUI.py
@@ -0,0 +1,112 @@
+"""
+Lux_MySQLBackup
+
+Copyright (c) 2024 LuxCoding
+
+This script is licensed under the MIT License.
+For full details, see the LICENSE file in the repository.
+"""
+# Import requiered Libs
+from tkinter import filedialog
+import tkinter
+import customtkinter
+import os
+import subprocess
+import sys
+from py.functions import load_config
+from py.functions import load_language
+from py.functions import translate
+from py.functions import decrypt_file
+
+# Save the Config and Translation in Varaibles
+config = load_config()
+translation, languages = load_language(config.get('language'))
+
+script_dir = os.path.dirname(__file__)
+
+# Function to Restart GUI
+def restart_GUI():
+    app.destroy()
+    subprocess.Popen([sys.executable, os.path.join(script_dir, 'Decrypt backup GUI.py')])
+
+# Select the Input File
+def select_input_file():
+    path = filedialog.askopenfilename(initialdir="/", title=translate('choose_input_file'), filetypes=(("sql files", "*.sql"),("all files", "*.*"))) 
+    input_file_entry.delete(0, customtkinter.END)
+    input_file_entry.insert(0, path)
+
+# Function to Select output Path
+def select_output_path():
+    path = filedialog.askdirectory(initialdir="/", title=translate('choose_output_path'))
+    output_file_path_entry.delete(0, customtkinter.END)
+    output_file_path_entry.insert(0, path)
+
+def decrypt_button_action():
+    try:
+        file_name = output_file_name_entry.get()
+        if not file_name.endswith('.sql'):
+            file_name += '.sql'
+
+        output_file_path = os.path.join(output_file_path_entry.get(), file_name)
+        decrypt_file(input_file_entry.get(), output_file_path, decrypt_password_entry.get())
+        restart_GUI()
+    except ValueError as e:
+        print(e)
+
+# Basic GUI Settings
+customtkinter.set_appearance_mode(config['background_theme'].lower())
+customtkinter.set_default_color_theme(config['theme_color'].lower())
+
+# Create GUI
+app = customtkinter.CTk()
+
+# Add Title and set Size
+app.title(translate('decrypt_gui_title'))
+app.geometry("500x280")
+
+# disable resize
+app.resizable(False, False)
+
+# Frame Where all Items will be in
+frame = customtkinter.CTkFrame(app)
+frame.pack(padx=20, pady=20)
+
+# Decryption Password Input
+decrypt_password_label = customtkinter.CTkLabel(frame, text=translate('decrypt_password'))
+decrypt_password_label.grid(row=1, column=0, padx=(10, 5), pady=10, sticky='w')
+
+decrypt_password_entry = customtkinter.CTkEntry(frame, width=200, height=30, border_width=2, corner_radius=10, show="*")
+decrypt_password_entry.grid(row=1, column=1, padx=(5, 10), pady=10, sticky='w')
+
+# File Input 
+input_file_label = customtkinter.CTkLabel(frame, text=translate('input_file'))
+input_file_label.grid(row=2, column=0, padx=(10, 5), pady=10, sticky='w')
+
+input_file_entry = customtkinter.CTkEntry(frame, width=200, height=30, border_width=2, corner_radius=10)
+input_file_entry.grid(row=2, column=1, padx=(5, 10), pady=10, sticky='w')
+
+input_file_button = customtkinter.CTkButton(frame, text='...', width=30, command=select_input_file)
+input_file_button.grid(row=2, column=1, padx=(210, 10), pady=10, sticky='w')
+
+# Output File name Input
+output_file_name_label = customtkinter.CTkLabel(frame, text=translate('Output_file_name'))
+output_file_name_label.grid(row=3, column=0, padx=(10, 5), pady=10, sticky='w')
+
+output_file_name_entry = customtkinter.CTkEntry(frame, width=200, height=30, border_width=2, corner_radius=10)
+output_file_name_entry.grid(row=3, column=1, padx=(5, 10), pady=10, sticky='w')
+
+# Output File Path Input
+output_file_path_label = customtkinter.CTkLabel(frame, text=translate('output_file_path'))
+output_file_path_label.grid(row=4, column=0, padx=(10, 5), pady=10, sticky='w')
+
+output_file_path_entry = customtkinter.CTkEntry(frame, width=200, height=30, border_width=2, corner_radius=10)
+output_file_path_entry.grid(row=4, column=1, padx=(5, 10), pady=10, sticky='w')
+
+output_file_path_button = customtkinter.CTkButton(frame, text='...', width=30, command=select_output_path)
+output_file_path_button.grid(row=4, column=1, padx=(210, 10), pady=10, sticky='w')
+
+# Add Save Button
+decrypt_button = customtkinter.CTkButton(master=app, text=translate('decrypt'), command=decrypt_button_action)
+decrypt_button.place(relx=0.5, rely=0.9, anchor=tkinter.CENTER)
+
+app.mainloop()
---
+++ Lux_MySQLBackup/py/functions.py
@@ -0,0 +1,243 @@
+"""
+Lux_MySQLBackup
+
+Copyright (c) 2024 LuxCoding
+
+This script is licensed under the MIT License.
+For full details, see the LICENSE file in the repository.
+"""
+# Import requiered Libs
+import ctypes
+from ctypes import wintypes
+import base64
+import json
+import os
+import subprocess
+import pymysql
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.primitives.hashes import SHA256
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives.ciphers.aead import AESGCM
+from cryptography.hazmat.primitives import padding
+from cryptography.hazmat.backends import default_backend
+
+# get the Script Path 
+script_dir = os.path.dirname(__file__)
+
+# Struckture for DATA_BLOB
+class DATA_BLOB(ctypes.Structure):
+    _fields_ = [
+        ("cbData", wintypes.DWORD),
+        ("pbData", ctypes.POINTER(ctypes.c_byte)),
+    ]
+
+# Funcstion helps to create DATA_BLOP ot of Bytes
+def _blob(data):
+    blob = DATA_BLOB()
+    blob.cbData = len(data)
+    blob.pbData = ctypes.cast(ctypes.create_string_buffer(data), ctypes.POINTER(ctypes.c_byte))
+    return blob
+
+# Function to Encryt Data with DAPAPI and returns Base64-coded Data
+def encrypt(data):
+    data_blob_in = _blob(data.encode('utf-8'))
+    data_blob_out = DATA_BLOB()
+
+    if ctypes.windll.crypt32.CryptProtectData(
+        ctypes.byref(data_blob_in),  # Data to encrypt
+        None,                        # Description (optional)
+        None,                        # Entropy key (optional)
+        None,                        # Reserved
+        None,                        # Prompt struct (optional)
+        0,                           # Flags
+        ctypes.byref(data_blob_out)  
+    ):
+        # Convert data in Base64 and return them
+        encrypted_data = ctypes.string_at(data_blob_out.pbData, data_blob_out.cbData)
+        ctypes.windll.kernel32.LocalFree(data_blob_out.pbData) 
+        return base64.b64encode(encrypted_data).decode('utf-8')
+    else:
+        raise ctypes.WinError()
+
+# Funcstion to Decrypt Base64-coded Data with DAPAPI
+def decrypt(encrypted_data):
+    try:
+        data_blob_in = _blob(base64.b64decode(encrypted_data))
+        data_blob_out = DATA_BLOB()
+
+        if ctypes.windll.crypt32.CryptUnprotectData(
+            ctypes.byref(data_blob_in),  # Encryped Data
+            None,                        # Description (optional)
+            None,                        # Entropy key (optional)
+            None,                        # Reserved
+            None,                        # Prompt struct (optional)
+            0,                           # Flags
+            ctypes.byref(data_blob_out)  
+        ):
+            # reutrn Decrypted data
+            decrypted_data = ctypes.string_at(data_blob_out.pbData, data_blob_out.cbData)
+            ctypes.windll.kernel32.LocalFree(data_blob_out.pbData)
+            return decrypted_data.decode('utf-8')
+        else:
+            raise ctypes.WinError()
+    except:
+        return ""
+    
+# Function to load the Config File
+def load_config():
+    try:
+        script_dir_one_back = os.path.normpath(script_dir[:-3])
+        with open(os.path.join(script_dir_one_back, 'json/settings.json'), 'r', encoding='utf-8') as f:
+            config = json.load(f)
+            return config
+    except FileNotFoundError:
+        print('File not fount')
+        return None
+    except json.JSONDecodeError as e:
+        print(f'Error by reading JSON-File: {e}')
+        return None
+    
+# fucntion to load the Language 
+def load_language(language):
+    try:
+        script_dir_one_back = os.path.normpath(script_dir[:-3])
+        with open(os.path.join(script_dir_one_back, 'json/language.json'), 'r', encoding='utf-8') as f:
+            data = json.load(f)  # JSON laden
+            languages = list(data.keys()) 
+            
+            language_data = data.get(language)
+            
+            if language_data is None:
+                print(f"the Language: '{language}' is not awaiable.")
+                return None, languages  
+            
+            return language_data, languages  
+    except FileNotFoundError:
+        print("File 'language.json' not found")
+        return None, []
+    except json.JSONDecodeError as e:
+        print(f'Error by Reading JSON-File: {e}')
+        return None, []
+    
+# function to translate 
+config = load_config()
+translation, languages = load_language(config.get('language'))
+
+# Function to Load Database
+def load_database():
+    try:
+        connection = pymysql.connect(
+            host=decrypt(config.get('hostname')),
+            user=decrypt(config.get('database_user')),
+            password=decrypt(config.get('database_password')),
+            charset='utf8mb4'  
+        )
+        return connection
+    except:
+        return None
+
+# Load Database
+connection = load_database()
+
+# Function to Execute SQL
+def SQL(Database, sql):
+    try:
+        with connection.cursor() as cursor:
+            if Database != '':
+                cursor.execute(f'USE {Database}')
+
+            cursor.execute(sql)
+            result = cursor.fetchall()
+
+            if cursor.rowcount > 0:
+                return result
+            else:
+                return ()
+    except Exception as e:
+        return ()  
+
+# Function to translate
+def translate(key, **kwargs):
+    text = translation.get(key, key)  
+    try:
+        return text.format(**kwargs) 
+    except KeyError:
+        return text
+
+# function to save the Confing to the Config file
+def save_config(config):
+    try:
+        script_dir_one_back = os.path.normpath(script_dir[:-3])
+        with open(os.path.join(script_dir_one_back, 'json/settings.json'), "w", encoding='utf-8') as f:
+            json.dump(config, f, indent=4)
+    except Exception as e:
+        print(translate('error_by_saving_config', e=e))
+        return None
+    
+# Funcstion to get all Databases
+def get_databases():
+    try:
+        result = SQL('', 'SHOW DATABASES')
+        if not result:
+            print(translate('error_reqest_database'))
+            return []
+        
+        databases = [item[0] for item in result]
+        return databases
+    except:
+        return []
+    
+# Fucnstion to Create key From Password 
+def derive_key(password, salt):
+    kdf = PBKDF2HMAC(
+        algorithm=SHA256(),
+        length=32,
+        salt=salt,
+        iterations=100000,
+        backend=default_backend()
+    )
+    return kdf.derive(password.encode())
+
+# Function to Encrypt File
+def encrypt_file(input_file, output_file, password):
+    salt = os.urandom(16)  # Generate Salt
+    key = derive_key(password, salt)
+    iv = os.urandom(16)  # Initialization vector
+    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
+    encryptor = cipher.encryptor()
+    padder = padding.PKCS7(algorithms.AES.block_size).padder()
+
+    # Read File and Encrypt
+    with open(input_file, 'rb') as f:
+        plaintext = f.read()
+    padded_data = padder.update(plaintext) + padder.finalize()
+    ciphertext = encryptor.update(padded_data)
+    final_data = encryptor.finalize()
+    ciphertext += final_data
+
+    # Writes File with Salt and IV
+    with open(output_file, 'wb') as f:
+        f.write(salt + iv + ciphertext)
+    
+
+
+# Function to Decrypt file
+def decrypt_file(input_file, output_file, password):
+    with open(input_file, 'rb') as f:
+        data = f.read()
+    salt = data[:16]  # Extract Salt
+    iv = data[16:32]  # Extract IV
+    ciphertext = data[32:]  # Extract Ciphertext
+    key = derive_key(password, salt)
+
+    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
+    decryptor = cipher.decryptor()
+    unpadder = padding.PKCS7(algorithms.AES.block_size).unpadder()
+
+    # Decrypt and remove padding
+    padded_plaintext = decryptor.update(ciphertext) + decryptor.finalize()
+    plaintext = unpadder.update(padded_plaintext) + unpadder.finalize()
+
+    # Write the Decrypted File
+    with open(output_file, 'wb') as f:
+        f.write(plaintext)
